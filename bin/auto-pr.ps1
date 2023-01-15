d46df8ea72547a0a65ac47d37f401fcc59b9ea08
Weidows <utsuko27@qq.com>
2023-01-15 18:14:30 +0800

    merge official template.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,9 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ScoopInstaller/Main:master"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+& $autopr -Dir $dir -Upstream $Upstream @Args
